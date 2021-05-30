78370fdf957ec3f1a43064a3ee2e5b619fe5a667
Mahesh Nanera <84780820+maheshnanera@users.noreply.github.com>
2021-05-30 10:39:28 +0530

    day 3
    dAY 3 task uploaded

---
+++ get.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+    <h1>DEMO PROJECT</h1>
+    <table>
+<tr>
+<td>Name :</td>
+<td><input type="text" name="txt1" required/></td>
+</tr>
+<tr>
+<td>Age :</td>
+<td><input type="number" name="txt2" required/></td>
+</tr>
+<tr>
+<td> Email :</td>
+<td><input type="email" name="txt3" required/></td>
+</tr>
+<tr>
+<td><input type="submit"></td>
+<td><input type="reset"></td>
+</tr>
+
+    </table>
+    
+    
+   
+    </form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+$a = $_POST['txt1'];
+$b = $_POST['txt2'];
+$c = $_POST['txt3'];
+echo "Hello my name is $a and my age is $b and email id is $c";
+   ?> 
+</body>
+</html>
